be2b8476c18590bd3cf4c58d37863eedfc1933b9
carmooo <joaorvcarmo@gmail.com>
2024-10-04 11:35:40 +0100

    add caching for film pages on letterboxd

--- go.mod
+++ go.mod
@@ -8,12 +8,15 @@ require (
 	github.com/antchfx/htmlquery v1.2.3 // indirect
 	github.com/antchfx/xmlquery v1.2.4 // indirect
 	github.com/antchfx/xpath v1.1.8 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-chi/chi/v5 v5.0.12 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly/v2 v2.1.0 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/redis/go-redis/v9 v9.6.1 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
--- scraper/letterboxd.go
+++ scraper/letterboxd.go
@@ -1,9 +1,14 @@
 package scraper
 
 import (
+	"context"
+	"encoding/json"
+	"os"
 	"regexp"
+	"time"
 
 	"github.com/gocolly/colly/v2"
+	"github.com/redis/go-redis/v9"
 )
 
 const LETTERBOXD_BASE_URL string = "https://letterboxd.com"
@@ -14,6 +19,13 @@ type StevenLuCustomMovie struct {
 	Imdb_id string `json:"imdb_id"`
 }
 
+var ctx = context.Background()
+
+var redisURL = os.Getenv("REDIS_URL")
+var rdb = redis.NewClient(&redis.Options{
+	Addr: redisURL,
+})
+
 func GetMoviesFromLetterboxd(slug string) []StevenLuCustomMovie {
 	r, _ := regexp.Compile(IMDB_REGEX_PATTERN)
 
@@ -21,15 +33,17 @@ func GetMoviesFromLetterboxd(slug string) []StevenLuCustomMovie {
 		colly.AllowedDomains("letterboxd.com"),
 	)
 
-	c.OnHTML(".col-main .poster-list .film-poster", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("data-target-link"))
-	})
-
-	c.OnHTML(".paginate-nextprev .next", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
-	})
-
 	var movies []StevenLuCustomMovie
+
+	c.OnHTML(".col-main .poster-list .film-poster", func(e *colly.HTMLElement) {
+		filmURL := e.Request.AbsoluteURL(e.Attr("data-target-link"))
+		if movie, found := getCachedFilmPage(filmURL); found {
+			movies = append(movies, movie)
+		} else {
+			e.Request.Visit(filmURL)
+		}
+	})
+
 	c.OnHTML("html", func(e *colly.HTMLElement) {
 		movie := StevenLuCustomMovie{}
 		imdbLink := e.ChildAttr("a[data-track-action=IMDb]", "href")
@@ -43,9 +57,29 @@ func GetMoviesFromLetterboxd(slug string) []StevenLuCustomMovie {
 		}
 		movie.Title = title
 		movies = append(movies, movie)
+		cacheFilmPage(e.Request.URL.String(), movie)
 	})
 
 	c.Visit(LETTERBOXD_BASE_URL + "/" + slug)
 
 	return movies
+}
+
+func cacheFilmPage(url string, movie StevenLuCustomMovie) {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	data, _ := json.Marshal(movie)
+	rdb.Set(ctx, url, data, 0)
+}
+
+func getCachedFilmPage(url string) (StevenLuCustomMovie, bool) {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	data, err := rdb.Get(ctx, url).Result()
+	if err != nil {
+		return StevenLuCustomMovie{}, false
+	}
+	var movie StevenLuCustomMovie
+	json.Unmarshal([]byte(data), &movie)
+	return movie, true
 }

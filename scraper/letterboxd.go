be77ce85d5dbb9e15170720770ecd99a2453b191
carmooo <joaorvcarmo@gmail.com>
2024-10-04 11:35:47 +0100

    add logs to letterboxd scraping

--- scraper/letterboxd.go
+++ scraper/letterboxd.go
@@ -3,6 +3,7 @@ package scraper
 import (
 	"context"
 	"encoding/json"
+	"log"
 	"os"
 	"regexp"
 	"time"
@@ -27,6 +28,7 @@ var rdb = redis.NewClient(&redis.Options{
 })
 
 func GetMoviesFromLetterboxd(slug string) []StevenLuCustomMovie {
+	log.Printf("Starting to scrape movies from Letterboxd for slug: %s", slug)
 	r, _ := regexp.Compile(IMDB_REGEX_PATTERN)
 
 	c := colly.NewCollector(
@@ -37,6 +39,7 @@ func GetMoviesFromLetterboxd(slug string) []StevenLuCustomMovie {
 
 	c.OnHTML(".col-main .poster-list .film-poster", func(e *colly.HTMLElement) {
 		filmURL := e.Request.AbsoluteURL(e.Attr("data-target-link"))
+		log.Printf("Found film poster with URL: %s", filmURL)
 		if movie, found := getCachedFilmPage(filmURL); found {
 			movies = append(movies, movie)
 		} else {
@@ -48,24 +51,30 @@ func GetMoviesFromLetterboxd(slug string) []StevenLuCustomMovie {
 		movie := StevenLuCustomMovie{}
 		imdbLink := e.ChildAttr("a[data-track-action=IMDb]", "href")
 		if imdbLink == "" {
+			log.Println("No IMDb link found, skipping")
 			return
 		}
 		movie.Imdb_id = r.FindStringSubmatch(imdbLink)[1]
 		title := e.ChildText(".headline-1")
 		if title == "" {
+			log.Println("No title found, skipping")
 			return
 		}
 		movie.Title = title
+		log.Printf("Scraped movie: %s (IMDb ID: %s)", movie.Title, movie.Imdb_id)
 		movies = append(movies, movie)
 		cacheFilmPage(e.Request.URL.String(), movie)
 	})
 
+	log.Printf("Visiting URL: %s/%s", LETTERBOXD_BASE_URL, slug)
 	c.Visit(LETTERBOXD_BASE_URL + "/" + slug)
 
+	log.Printf("Finished scraping movies for slug: %s", slug)
 	return movies
 }
 
 func cacheFilmPage(url string, movie StevenLuCustomMovie) {
+	log.Printf("Caching movie: %s (URL: %s)", movie.Title, url)
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 	data, _ := json.Marshal(movie)
@@ -73,13 +82,16 @@ func cacheFilmPage(url string, movie StevenLuCustomMovie) {
 }
 
 func getCachedFilmPage(url string) (StevenLuCustomMovie, bool) {
+	log.Printf("Checking cache for URL: %s", url)
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 	data, err := rdb.Get(ctx, url).Result()
 	if err != nil {
+		log.Printf("Cache miss for URL: %s", url)
 		return StevenLuCustomMovie{}, false
 	}
 	var movie StevenLuCustomMovie
 	json.Unmarshal([]byte(data), &movie)
+	log.Printf("Cache hit for URL: %s", url)
 	return movie, true
 }

fbe22dd8a1deda5ccdec42646be8196946ff42aa
carmooo <joaorvcarmo@gmail.com>
2024-10-04 11:35:34 +0100

    add health endpoint

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
 	r.Get("/letterboxd/*", handler.HandleLetterboxd)
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("All well under the stairwell"))
+	})
+
 	http.ListenAndServe(":3000", r)
 }
